438991d752ec69c3b5522c531d91ed059fae9387
ruezomon <ruezgar.guenduez@gmail.com>
2026-09-09 22:37:19 +0200

    feat: add delta time function for frame manager

--- include/FrameHandler.hpp
+++ include/FrameHandler.hpp
@@ -1,15 +1,28 @@
+#pragma once
+
+#include <thread>
+
 #include "Image.hpp"
+#include "GameHandler.hpp"
 
 namespace ruezo {
     class FrameHandler {
     private:
         ruezo::Image img;
         int* pixelBuffer;
+        ruezo::GameHandler* gh = nullptr;
+        int x = 0;
+        int y = 0;
+        double elapsed = 0.0f;
+        std::thread* t = nullptr;
+        bool t_flag = true;
 
     public:
         FrameHandler(int width, int height);
         ~FrameHandler();
         int* getPixelBuffer();
         ruezo::Image& getImage();
+        void initGame(ruezo::GameHandler* gh);
+        double getDeltaTime(); // returns delta time in seconds
     };
 }
--- src/FrameHandler.cpp
+++ src/FrameHandler.cpp
@@ -1,10 +1,21 @@
 #include "FrameHandler.hpp"
+#include "InitializeException.hpp"
 
 ruezo::FrameHandler::FrameHandler(int width, int height) : 
-    img(ruezo::Image(height, width)), pixelBuffer(new int[width * height * 4]) {}
+    img(ruezo::Image(height, width)), pixelBuffer(new int[width * height * 4]) {
+        this->t = new std::thread([this]() {
+            while (this->t_flag) {
+                this->elapsed += 0.01;
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            }
+        });
+        this->t->detach();
+    }
 
 ruezo::FrameHandler::~FrameHandler() {
     delete[] this->pixelBuffer;
+    this->t_flag = false;
+    delete this->t;
 }
 
 ruezo::Image& ruezo::FrameHandler::getImage() {
@@ -13,4 +24,15 @@ ruezo::Image& ruezo::FrameHandler::getImage() {
 
 int* ruezo::FrameHandler::getPixelBuffer() {
     return this->pixelBuffer;
+}
+
+void ruezo::FrameHandler::initGame(ruezo::GameHandler* gh) {
+    if (this->gh == nullptr) this->gh = gh;
+    else throw ruezo::InitializeException("GameHandler can only be initialized once", 2);
+}
+
+double ruezo::FrameHandler::getDeltaTime() {
+    double r = this->elapsed;
+    this->elapsed = 0.0f;
+    return r;
 }
